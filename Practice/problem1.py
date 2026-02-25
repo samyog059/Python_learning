7d28159dcdc0d6b03b5361561dd324a0456d134c
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-02-25 20:33:59 +0545

    printed a poem

---
+++ Practice/problem1.py
@@ -0,0 +1,19 @@
+print("""arabaji lailai
+mama aaye ghoda
+maiju aain doli
+papa lyain soli
+bubu maam supukka
+kafal gedi kutukka
+biralo bhanchha myau myau
+agulto bhanchha uchhittya
+
+====break====
+
+tarabaji lailai
+mama aaye ghoda
+maiju aain doli
+papa lyain soli
+bubu maam supukka
+kafal gedi kutukka
+biralo bhanchha myau myau
+agulto bhanchha uchhittyau""")
