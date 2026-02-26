36bea0ea60fd4478810cd52fb6f5c7230969314f
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-02-26 20:03:15 +0545

    comment added rn

--- Practice/problem3.py
+++ Practice/problem3.py
@@ -2,5 +2,6 @@ import os
 directory_path ='/'
 contents = os.listdir(directory_path)
 for item in contents:
+    # prints the name of the file or directory
     print(item)
     # print(os.path.join(directory_path, item))
