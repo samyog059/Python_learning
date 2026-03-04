cedf43f0601e2b17035dfcf93264a278e8b08ab0
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-04 21:06:25 +0545

    my system fad denge

--- Practice/Vars_data/prob7.py
+++ Practice/Vars_data/prob7.py
@@ -4,3 +4,4 @@ if age>=18:
     print(f"You are eligible to vote. Mr. {name}")
     else:
         print(f"Hey {name}, you are not eligible to vote yet. Please wait until you turn 18.")
+        print("Thank you for using the voting eligibility checker.")
