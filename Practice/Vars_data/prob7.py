03d62c2d3c566df117213f211236738c13e748af
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-04 21:08:13 +0545

    line mila ko

--- Practice/Vars_data/prob7.py
+++ Practice/Vars_data/prob7.py
@@ -3,6 +3,6 @@ age = int (input("Enter your age:"))
 name = input('Enter your name:')
 if age>=18:
     print(f"You are eligible to vote. Mr. {name}")
-    else:
-        print(f"Hey {name}, you are not eligible to vote yet. Please wait until you turn 18.")
-        print("Thank you for using the voting eligibility checker.")
+else:
+    print(f"Hey {name}, you are not eligible to vote yet. Please wait until you turn 18.")
+    print("Thank you for using the voting eligibility checker.")
