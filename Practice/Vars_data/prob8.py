d33779761d5848ae48f3633c70abf0a7f0aa75a6
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-05 07:09:44 +0545

    problem solved

--- Practice/Vars_data/prob8.py
+++ Practice/Vars_data/prob8.py
@@ -1,2 +1,5 @@
 # Program to calculate the area of a circle
 import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print(f"The area of the circle with radius {radius} is: {area:.2f}")
