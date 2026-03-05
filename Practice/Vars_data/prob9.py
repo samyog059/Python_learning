c7e74f0bc94352324efb365f06a95a656046c7e0
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-05 07:10:49 +0545

    completed the problem

--- Practice/Vars_data/prob9.py
+++ Practice/Vars_data/prob9.py
@@ -1,1 +1,5 @@
 # program to calculate the area of a rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))  
+area = length * width
+print(f"The area of the rectangle with length {length} and width {width} is: {area:.2f}")
