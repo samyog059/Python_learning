abb6705d8682fc336c94f88a5c7d9a41507b2f17
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-06 19:12:44 +0545

    shanta nepal

--- Practice/Vars_data/prob9.py
+++ Practice/Vars_data/prob9.py
@@ -3,4 +3,4 @@ length = float(input("Enter the length of the rectangle: "))
 width = float(input("Enter the width of the rectangle: "))  
 area = length * width
 print(f"The area of the rectangle with length {length} and width {width} is: {area:.2f}")
-# Success is like being pregnant, everyone says congratulations but nobody knows how many times you were screwed before you got there.
+# Success is like being pregnant, everyone says congratulations but nobody knows how many times you were screwed before you got there....
