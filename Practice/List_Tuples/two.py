a98ba0f642c5c39c45a063264f7c59c19872a00b
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-17 21:06:34 +0545

    jpr

--- Practice/List_Tuples/two.py
+++ Practice/List_Tuples/two.py
@@ -1,2 +1,5 @@
 kings_of_nepal=("Prithvi Narayan Shah", "Pratap Singh Shah", "Nara Bhupal Shah", "Rana Bahadur Shah", "Girvan Yuddha Bikram Shah", "Rajendra Bikram Shah", "Surendra Bikram Shah", "Trailokya Bir Bikram Shah", "Prithvi Bir Bikram Shah", "Tribhuvan Bir Bikram Shah", "Mahendra Bir Bikram Shah", "Birendra Bir Bikram Shah", "Dipendra Bir Bikram Shah", "Gyanendra Bir Bikram Shah")    
 print(kings_of_nepal)
+primeministers_of_nepal=("Bishweshwar Prasad Koirala", "Matrika Prasad Koirala", "Subarna Shamsher Rana", "Matrika Prasad Koirala", "Bishweshwar Prasad Koirala", "Tulsi Giri", "Surya Bahadur Thapa", "Kirti Nidhi B
+ista", "Marshal Mohan Shamsher Jang Bahadur Rana", "Surya Bahadur Thapa", "Lokendra Bahadur Chand", "Sher Bahadur Deuba", "Pushpa Kamal Dahal", "Khadga Prasad Sharma Oli", "Sher Bahadur Deuba", "Khadga Prasad Sharma Oli", "Sher Bahadur Deuba")
+print(primeministers_of_nepal)
