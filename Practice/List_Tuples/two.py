399f0d3772529aed01b220ead8c896974ac7fcb4
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-17 21:07:30 +0545

    gomygo

--- Practice/List_Tuples/two.py
+++ Practice/List_Tuples/two.py
@@ -3,3 +3,5 @@ print(kings_of_nepal)
 primeministers_of_nepal=("Bishweshwar Prasad Koirala", "Matrika Prasad Koirala", "Subarna Shamsher Rana", "Matrika Prasad Koirala", "Bishweshwar Prasad Koirala", "Tulsi Giri", "Surya Bahadur Thapa", "Kirti Nidhi B
 ista", "Marshal Mohan Shamsher Jang Bahadur Rana", "Surya Bahadur Thapa", "Lokendra Bahadur Chand", "Sher Bahadur Deuba", "Pushpa Kamal Dahal", "Khadga Prasad Sharma Oli", "Sher Bahadur Deuba", "Khadga Prasad Sharma Oli", "Sher Bahadur Deuba")
 print(primeministers_of_nepal)
+Best_leaders_of_rsp=("Rabi Lamichhane", "Balen shah")
+print(Best_leaders_of_rsp)
