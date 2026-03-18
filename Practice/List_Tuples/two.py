8d79a3023e0512b5a614c65350706e0301a07340
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-18 21:23:21 +0545

    add

--- Practice/List_Tuples/two.py
+++ Practice/List_Tuples/two.py
@@ -2,3 +2,4 @@ Religions= ["Hinduism", "Islam", "Christianity", "Sikhism", "Buddhism", "Judaism
 print(Religions[0]) // Output: Hinduism
 print(Religions[1]) // Output: Islam
 print(Religions[2]) // Output: Christianity
+print(Religions[3]) // Output: Sikhism
