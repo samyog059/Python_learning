a1f8af84c0b1f4e371427b6390eac9a3abfe8cef
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-23 20:40:12 +0545

    SI and CI

--- Practice/Random/practice.py
+++ Practice/Random/practice.py
@@ -4,3 +4,9 @@ rate = float(input("Enter the rate of interest: "))
 time = float(input("Enter the time period: "))  
 Simple_Interest = (principal * rate * time) / 100
 print("The Simple Interest is:", Simple_Interest)
+# Compound Interest Calculator
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter the time period: "))
+compound_interest = principal * (1 + rate/100) ** time - principal
+print("The Compound Interest is:", compound_interest)               
