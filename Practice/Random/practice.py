2f73cccf77e8421417d8a21b68b1933702e32687
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-03-23 20:39:30 +0545

    command

--- Practice/Random/practice.py
+++ Practice/Random/practice.py
@@ -2,3 +2,5 @@
 principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the rate of interest: "))
 time = float(input("Enter the time period: "))  
+Simple_Interest = (principal * rate * time) / 100
+print("The Simple Interest is:", Simple_Interest)
