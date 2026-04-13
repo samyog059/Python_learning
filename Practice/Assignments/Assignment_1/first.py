8cb5a7467638164080b9061f3a8a2c4433bee21f
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-13 13:30:27 +0545

    homweorl

---
+++ Practice/Assignments/Assignment_1/first.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+faculty = input("Enter your faculty:")
+print(f"Hello {name}, you are {age} years old and you are in the {faculty} faculty.")
