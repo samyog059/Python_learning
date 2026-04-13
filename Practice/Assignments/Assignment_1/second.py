dd2a42e4666556c3f67780b357c2833317efc61e
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-13 13:38:24 +0545

    comit

---
+++ Practice/Assignments/Assignment_1/second.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number:") )
+b=int(input("Enter the second number:") )
+sum=a+b
+mul=a*b
+div=a/b
+sub=a-b
+print(f"The sum of {a} and {b} is: {sum}")
+print(f"The multiplication of {a} and {b} is: {mul}")
+print(f"The division of {a} and {b} is: {div}")
+print(f"The subtraction of {a} and {b} is: {sub}")
