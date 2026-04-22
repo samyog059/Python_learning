3016249679308776372009f284a9b84f21210476
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-22 06:25:10 +0545

    SEE

--- Practice/Assignments/Assignment_cw/sets.py
+++ Practice/Assignments/Assignment_cw/sets.py
@@ -4,4 +4,8 @@ s={1,2,3,4}
 s2=set([1,2,2,3])
 #Adding elements in set
 s.add(4)
+#Removing elements
+s.remove(1)#error if element not found
+s.discard(5)#no error if element not found
+s.pop()
 print(s)
