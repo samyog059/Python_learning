e37c0a161d17e19f2baf5dec51be3a5eb1c7ba69
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-23 06:43:43 +0545

    yadai salala

---
+++ Practice/Object Oriented Programming/anoth.py
@@ -0,0 +1,9 @@
+class Info:
+    def __init__(self,name,email,phone):
+        self.name=name
+        self.email=email
+        self.phone=phone
+i1=Info("Samyog","samyogpangeni4@gmail.com",9848262365)
+print(i1.name)
+print(i1.phone)
+print(i1.email)
