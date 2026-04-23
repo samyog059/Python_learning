6c840bb5348ca66c9751c3a88b2a31825e1fbb4e
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-23 06:34:02 +0545

    hw

--- Practice/Object Oriented Programming/class.py
+++ Practice/Object Oriented Programming/class.py
@@ -1,5 +1,5 @@
-class Car:
-    def start(self):
+class Car: #creating a class named Car
+    def start(self): #creating a method named start
         print("car Started")
-my_car=Car()
-my_car.start()
+my_car=Car() #creating an object named my_car of the class Car
+my_car.start() #calling the start method using the my_car object
