0f219d270344de46660f3cee73938bccc9206f73
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-24 06:35:22 +0545

    ...

--- Practice/Object Oriented Programming/self.py
+++ Practice/Object Oriented Programming/self.py
@@ -1,7 +1,7 @@
 class Person:
     def __init__(self, name):
         self.name=name
-    def show(self):
+    def show(self): # show is a method that prints the name of the person. It uses the self parameter to access the name attribute of the class.
         print(self.name)
 p=Person("Hari")
 p.show()
