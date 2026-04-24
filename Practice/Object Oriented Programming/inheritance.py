725e7561d52ebba973653631bda22e2cf96bb7d1
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-24 06:43:22 +0545

    pelsiha

---
+++ Practice/Object Oriented Programming/inheritance.py
@@ -0,0 +1,9 @@
+class Grandpa:
+    def worships(self):
+        print("He worships gods")
+class Grandson(Grandpa):
+    def spiritual(self):
+        print("He is spiritual")
+g=Grandson()
+g.worships()
+g.spiritual()
