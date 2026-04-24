c1a938645a5c448d0dc9004462b53e95b53bf98a
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-24 06:53:01 +0545

    Assignment

---
+++ Practice/Object Oriented Programming/ply.py
@@ -0,0 +1,17 @@
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat meows")
+
+# Polymorphism in action
+animals = [Dog(), Cat(), Animal()]
+
+for a in animals:
+    a.speak()
