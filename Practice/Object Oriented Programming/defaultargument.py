ccbc696297eb2a77d02980c3c2cc35963ade2d54
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-28 06:28:19 +0545

    setidovan

---
+++ Practice/Object Oriented Programming/defaultargument.py
@@ -0,0 +1,5 @@
+def greet(name="Guest"): # This function takes a name as input and prints a greeting message. If no name is provided, it defaults to "Guest".
+    print("Hello", name) # The print function is used to display the greeting message on the console.
+greet()
+greet("Samyog ")
+
