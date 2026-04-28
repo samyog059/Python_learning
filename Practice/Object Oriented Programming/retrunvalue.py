f39339977ac189a42287c54973504ee02be13199
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-04-28 06:33:09 +0545

    baisa joban

--- Practice/Object Oriented Programming/retrunvalue.py
+++ Practice/Object Oriented Programming/retrunvalue.py
@@ -1,4 +1,4 @@
-def add(a,b):
-    return a + b
-result = add(5, 3)
-print(f"The result is: {result}")
+def add(a,b): # This function takes two arguments, a and b, and returns their sum.
+    return a + b # The return statement is used to send the result back to the caller of the function.
+result = add(5, 3) 
+print(f"The result is: {result}") 
