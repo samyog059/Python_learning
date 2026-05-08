505efc9c947a2d9664511b5b3949e6fb0ede2c52
Samyog Pangeni <samyogpangeni4@gmail.com>
2026-05-08 06:32:03 +0545

    .......

--- Practice/Database/db.py
+++ Practice/Database/db.py
@@ -9,3 +9,10 @@ age INTEGER NOT NULL)
 conn.commit()
 print("Table created successfully")
 conn.close()
+# show table content
+conn = sqlite3.connect('college.db')
+cur=conn.cursor()
+cur.execute("SELECT * FROM student")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
